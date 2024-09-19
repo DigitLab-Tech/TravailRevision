e02f86f5f2076cd8913d1a360168401095fd9008
Cedrick Plante <cedrick.plante@gmail.com>
2024-09-19 17:26:25 -0400

    fixing validator

--- validator.js
+++ validator.js
@@ -1,16 +1,13 @@
 export function validator(object1, object2){
     if(object1 && object2){
         for(let propertyName of Object.getOwnPropertyNames(object1)){
-            if(object2[propertyName] && Array.isArray(object1[propertyName]) && Array.isArray(object2[propertyName])){
+            if(Array.isArray(object1[propertyName]) && Array.isArray(object2[propertyName])){
                 for(let i = 0; i < object1[propertyName].length; i++){
                     if(object1[propertyName][i] !== object2[propertyName][i]){
                         return false;
                     }
                 }
-                return true;
-            }
-
-            if(!object2[propertyName] || object1[propertyName] !== object2[propertyName]){
+            }else if(!object2[propertyName] || object1[propertyName] !== object2[propertyName]){
                 return false;
             }
         }
@@ -22,10 +19,7 @@ export function validator(object1, object2){
                         return false;
                     }
                 }
-                return true;
-            }
-
-            if(!object1[propertyName] || object1[propertyName] !== object2[propertyName]){
+            } else if(!object1[propertyName] || object1[propertyName] !== object2[propertyName]){
                 return false;
             }
         }
