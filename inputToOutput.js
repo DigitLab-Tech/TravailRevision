1fac3877d0bf64f960a05872f56fdb9214b6f670
Cedrick <cedrick@nabupro.com>
2024-09-23 08:35:05 -0400

    rollback

--- inputToOutput.js
+++ inputToOutput.js
@@ -1,3 +1,31 @@
 export function inputToOutput(input){
-    return {};
+    const splitInput = input
+    .replace('<', '')
+    .replace(' />', '')
+    .replaceAll('data-', '')
+    .replaceAll('"', '')
+    .split(' ');
+
+    const object = {type: splitInput[0]};
+
+    for(let i = 1; i < splitInput.length; i++){
+        let [key, value] = splitInput[i].split('=');
+
+        if(!isNaN(value)){
+            value = parseInt(value);
+        }else if(value.includes('|')){
+            value = value.split('|');
+        }
+
+        if(key.includes('-')){
+            const charIndex = key.indexOf('-');
+
+            key = key.replace('-', '');
+            key = key.slice(0, charIndex) + key[charIndex].toUpperCase() + key.slice(charIndex + 1, key.length);
+        }
+
+        object[key] = value;
+    }
+
+    return object;
 }
