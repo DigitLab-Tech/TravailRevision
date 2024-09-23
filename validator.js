a4fd10bd5679b42a967b9ee808db21c7b0fb62a2
Cedrick <cedrick@nabupro.com>
2024-09-23 08:32:16 -0400

    initial commit

--- validator.js
+++ validator.js
@@ -1,31 +1,3 @@
 export function validator(object1, object2){
-    if(object1 && object2){
-        for(let propertyName of Object.getOwnPropertyNames(object1)){
-            if(Array.isArray(object1[propertyName]) && Array.isArray(object2[propertyName])){
-                for(let i = 0; i < object1[propertyName].length; i++){
-                    if(object1[propertyName][i] !== object2[propertyName][i]){
-                        return false;
-                    }
-                }
-            }else if(!object2[propertyName] || object1[propertyName] !== object2[propertyName]){
-                return false;
-            }
-        }
-
-        for(let propertyName of Object.getOwnPropertyNames(object2)){
-            if(object1[propertyName] && Array.isArray(object1[propertyName]) && Array.isArray(object2[propertyName])){
-                for(let i = 0; i < object1[propertyName].length; i++){
-                    if(object1[propertyName][i] !== object2[propertyName][i]){
-                        return false;
-                    }
-                }
-            } else if(!object1[propertyName] || object1[propertyName] !== object2[propertyName]){
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    return false;
+    return object1 === object2;
 }
